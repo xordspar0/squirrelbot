3355a39b42b24164eaf36ae037a209fda3585022
Jordan Christiansen <xordspar0@gmail.com>
2017-06-05 16:21:13 -0500

    Download Youtube videos

--- bot/handlers.go
+++ bot/handlers.go
@@ -5,26 +5,47 @@ import (
 
 	"errors"
 	"fmt"
+	"os/exec"
 )
 
+// handleYoutube takes Youtube url strings, downloads them, and sends a message
+// back to the user.
 func handleYoutube(message map[string]interface{}, url, token string) error {
 	recipient, err := getID(message)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(recipient) //debug
+	// Download the video with youtube-dl.
+	// Frist, make sure that youtube-dl is installed.
+	_, err = exec.LookPath("youtube-dl")
+	if err != nil {
+		_ = telegram.SendMessage(
+			recipient,
+			"Error: The server does not have youtube-dl installed on it.",
+			token,
+		)
+		return err
+	}
 
-	return telegram.SendMessage(
+	cmd := exec.Command("youtube-dl", url)
+	err = cmd.Run()
+
+	// Finally, send a message back to the user.
+	err = telegram.SendMessage(
 		recipient,
 		"I saved your Youtube video.",
 		token,
 	)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func handleLink(message map[string]interface{}, url, token string) error {
 	recipient, err := getID(message)
-
 	if err != nil {
 		return err
 	}
