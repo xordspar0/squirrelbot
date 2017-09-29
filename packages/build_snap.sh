54f96049f62efb286fb1db2ee17e75de788d69c2
Jordan Christiansen <xordspar0@gmail.com>
2017-09-29 21:49:15 +0000

    Added youtube-dl files into the snap package

--- packages/build_snap.sh
+++ packages/build_snap.sh
@@ -1,15 +1,18 @@
+#!/bin/sh
+set -e
+
 version="$1"
 
 cd $(dirname $0)
 
 cp snap/snapcraft.yaml.template snap/snapcraft.yaml
 
-sed -i '' "s/{{version}}/$version/" packages/snap/snapcraft.yaml
+sed -i "s/{{version}}/$version/" snap/snapcraft.yaml
 
-if [ $version == 'devel' ]; then
-  sed -i '' "s/{{grade}}/devel/" packages/snap/snapcraft.yaml
+if [ $version = 'devel' ]; then
+  sed -i "s/{{grade}}/devel/" snap/snapcraft.yaml
 else
-  sed -i '' "s/{{grade}}/stable/" packages/snap/snapcraft.yaml
+  sed -i "s/{{grade}}/stable/" snap/snapcraft.yaml
 fi
 
 snapcraft
