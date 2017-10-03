5141fc9a0abce4de0adaf66f471f2c8657e372fa
Jordan Christiansen <xordspar0@gmail.com>
2017-10-02 22:30:40 -0500

    Added youtube-dl function to grab video descriptions

--- youtubedl/youtubedl.go
+++ youtubedl/youtubedl.go
@@ -24,27 +24,8 @@ func Download(url, directory, prefix string) error {
 		fmt.Sprintf("%s/%s%s.%s", directory, prefix, "%(title)s", "%(ext)s"),
 		url,
 	)
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return errors.New("youtubedl: Failed to download video: " + err.Error())
-	}
 
-	err = cmd.Start()
-	if err != nil {
-		return errors.New("youtubedl: Failed to download video: " + err.Error())
-	}
-
-	// Catch any error messgages while the process is running.
-	errMessages, err := ioutil.ReadAll(stderr)
-	if err != nil {
-		return errors.New("youtubedl: " + err.Error())
-	}
-	err = cmd.Wait()
-	if err != nil {
-		return errors.New("External program youtube-dl: \n" + string(errMessages))
-	}
-
-	return nil
+	return downloadFile(cmd)
 }
 
 // Download uses youtube-dl to download the thumbnail of a video.
@@ -60,14 +41,41 @@ func DownloadThumbnail(url, directory, prefix string) error {
 		fmt.Sprintf("%s/%s%s-thumb.%s", directory, prefix, "%(title)s", "%(ext)s"),
 		url,
 	)
+
+	return downloadFile(cmd)
+}
+
+// GetTitle uses youtube-dl to get the name of a video.
+func GetTitle(url string) (videoTitle string) {
+	cmd := exec.Command(
+		"youtube-dl",
+		"--get-title",
+		url,
+	)
+
+	return getCmdOutput(cmd)
+}
+
+// GetDescription uses youtube-dl to get the description of a video.
+func GetDescription(url string) string {
+	cmd := exec.Command(
+		"youtube-dl",
+		"--get-description",
+		url,
+	)
+
+	return getCmdOutput(cmd)
+}
+
+func downloadFile(cmd *exec.Cmd) error {
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
-		return errors.New("youtubedl: Failed to download thumbnail: " + err.Error())
+		return errors.New("youtubedl: Failed to download video: " + err.Error())
 	}
 
 	err = cmd.Start()
 	if err != nil {
-		return errors.New("youtubedl: Failed to download thumbnail: " + err.Error())
+		return errors.New("youtubedl: Failed to download video: " + err.Error())
 	}
 
 	// Catch any error messgages while the process is running.
@@ -83,37 +91,29 @@ func DownloadThumbnail(url, directory, prefix string) error {
 	return nil
 }
 
-// GetTitle uses youtube-dl to get the name of a video.
-func GetTitle(url string) (videoTitle string) {
-	cmd := exec.Command(
-		"youtube-dl",
-		"--get-title",
-		url,
-	)
-
+// getCmdResponse runs a command and returns its output. If there are any errors
+// in the command, then just return an empty string.
+func getCmdOutput(cmd *exec.Cmd) string {
 	stdout, err := cmd.StdoutPipe()
 	var out []byte
 	if err != nil {
-		return
+		return ""
 	}
 
 	err = cmd.Start()
 	if err != nil {
-		return
+		return ""
 	}
 
 	out, err = ioutil.ReadAll(stdout)
 	if err != nil {
-		return
+		return ""
 	}
 
 	err = cmd.Wait()
 	if err != nil {
-		return
+		return ""
 	}
 
-	videoTitle = strings.TrimSpace(string(out))
-
-	// If there are any errors getting the video name, then just leave it blank.
-	return
+	return strings.TrimSpace(string(out))
 }
